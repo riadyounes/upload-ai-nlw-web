068b749b1183228122665ff6c340d38a8105c523
Riad Younes <riad.younes@hotmail.com>
2023-12-11 15:37:23 -0300

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,13 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      port: 5173,
+    },
+  },
   optimizeDeps: {
     exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
   },
